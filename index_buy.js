88a261f6cfcc332d8f136bcd4ec710237c0e00b6
CHEN-CHI-TIN <59937482+CHEN-CHI-TIN@users.noreply.github.com>
2021-03-10 18:37:43 +0800

    Update index_buy.js

--- index_buy.js
+++ index_buy.js
@@ -28,14 +28,14 @@ if (pairFront == "usdt") amountRound = 3;
 if (pairFront == "btc") amountRound = 8;
 
 setInterval(() => {
+    //計算總資產
+    getTotalBalance();
+    let total = totalBalance["total"];
+    let balanceFront = totalBalance["front"];
+    let balanceBack = totalBalance["back"];
+
     //查看歷史資料，並開始策略
     get(o.optionHistoryData(pair, resolution, timeAmount, timeUnit)).then((resData) => {
-        //計算總資產
-        getTotalBalance();
-        let total = totalBalance["total"];
-        let balanceFront = totalBalance["front"];
-        let balanceBack = totalBalance["back"];
-
         //使用歷史數據計算相關指標
         let data = resData["data"];
         let fractalUp = o.getFractalUP(o.getHighData(data));
@@ -52,141 +52,110 @@ setInterval(() => {
         console.log("當前價格：", currentPrice, "|", "上分形：", fractalUp, "|", "下分形：", fractalDown);
         console.log("總資產：", total, "|", `${pairFront}：`, balanceFront, "|", `${pairBack}：`, balanceBack);
 
-        /**
-         * 追蹤多頭平倉之空單
-         */
-        if (watchOffsetBuy == true) {
-            //查看所有合約
-            get(o.optionAllOrders(pair, apiKey, apiSecret, {
-                identity: email,
-                nonce: Date.now()
-            })).then((resData) => {
-                //如果尚有空單，刪除漏單並重新下單
-                if (o.hasSellContract(resData["data"]) == true) {
-                    //刪除漏單
-                    del(o.optionDeleteOrder(apiKey, apiSecret, {
-                        identity: email,
-                        nonce: Date.now()
-                    })).then((resData) => {
-                        console.log("刪除多頭平倉之漏單");
-                        console.log(resData);
-                        //重新下單
-                        console.log("重新下單");
-                        order("SELL", lastSellAmount, currentPrice);
-                    });
-                } else {
-                    console.log("確認已多頭平倉");
-                    watchOffsetBuy = false;
-                    sumBuy++;
-                }
-            });
-        }
+        if (total != undefined) {
+            /**
+             * 追蹤多頭平倉之空單
+             */
+            if (watchOffsetBuy == true) {
+                //查看所有合約
+                get(o.optionAllOrders(pair, apiKey, apiSecret, {
+                    identity: email,
+                    nonce: Date.now()
+                })).then((resData) => {
+                    //如果尚有空單，刪除漏單並重新下單
+                    if (o.hasSellContract(resData["data"]) == true) {
+                        //刪除漏單
+                        del(o.optionDeleteOrder(apiKey, apiSecret, {
+                            identity: email,
+                            nonce: Date.now()
+                        })).then((resData) => {
+                            console.log("刪除多頭平倉之漏單");
+                            console.log(resData);
+                            //重新下單
+                            console.log("重新下單");
+                            order("SELL", lastSellAmount, currentPrice);
+                        });
+                    } else {
+                        console.log("確認已多頭平倉");
+                        watchOffsetBuy = false;
+                        sumBuy++;
+                    }
+                });
+            }
 
-        /**
-         * 追蹤做多之多單
-         */
-        if (watchBuy == true) {
-            //查看所有合約
-            get(o.optionAllOrders(pair, apiKey, apiSecret, {
-                identity: email,
-                nonce: Date.now()
-            })).then((resData) => {
-                //如果尚有多單，刪除漏單並重新下單
-                if (o.hasBuyContract(resData["data"]) == true) {
-                    //刪除漏單
-                    del(o.optionDeleteOrder(apiKey, apiSecret, {
-                        identity: email,
-                        nonce: Date.now()
-                    })).then((resData) => {
-                        console.log("刪除做多之漏單");
-                        console.log(resData);
-                        //重新下單
-                        console.log("重新下單");
-                        order("BUY", lastBuyAmount, currentPrice);
-                    });
-                } else {
-                    console.log("確認已做多");
-                    watchBuy = false;
-                }
-            });
-        }
+            /**
+             * 追蹤做多之多單
+             */
+            if (watchBuy == true) {
+                //查看所有合約
+                get(o.optionAllOrders(pair, apiKey, apiSecret, {
+                    identity: email,
+                    nonce: Date.now()
+                })).then((resData) => {
+                    //如果尚有多單，刪除漏單並重新下單
+                    if (o.hasBuyContract(resData["data"]) == true) {
+                        //刪除漏單
+                        del(o.optionDeleteOrder(apiKey, apiSecret, {
+                            identity: email,
+                            nonce: Date.now()
+                        })).then((resData) => {
+                            console.log("刪除做多之漏單");
+                            console.log(resData);
+                            //重新下單
+                            console.log("重新下單");
+                            order("BUY", lastBuyAmount, currentPrice);
+                        });
+                    } else {
+                        console.log("確認已做多");
+                        watchBuy = false;
+                    }
+                });
+            }
 
-        /**
-         * 上唇 < 齒，多頭平倉
-         */
-        if (alligatorUp < alligatorMiddel && buy == true && watchBuy == false) {
-            console.log("上唇 < 齒，多頭平倉");
-            //平倉要用pairFront賣，使用pairFront資產balanceFront
-            let amount = balanceFront * amountPercent; //每次購買amountPercent
-            amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
-            lastSellAmount = amount;
-            order("SELL", amount, currentPrice); //下單
-            buy = false; //做多結束
-            watchOffsetBuy = true; //開始監視平倉之空單
-        }
+            /**
+             * 上唇 < 齒，多頭平倉
+             */
+            if (alligatorUp < alligatorMiddel && buy == true && watchBuy == false) {
+                console.log("上唇 < 齒，多頭平倉");
+                //平倉要用pairFront賣，使用pairFront資產balanceFront
+                let amount = balanceFront * amountPercent; //每次購買amountPercent
+                amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
+                lastSellAmount = amount;
+                order("SELL", amount, currentPrice); //下單
+                buy = false; //做多結束
+                watchOffsetBuy = true; //開始監視平倉之空單
+            }
 
-        /**
-         * 上唇 > 齒 > 下巴 & 下分形 > 下巴，為相對低點；做多
-         */
-        if (alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && fractalDown > alligatorDown && buy == false) {
-            console.log("上唇 > 齒 > 下巴 & 下分形 > 下巴，為相對低點；做多");
-            //多單要用pairBack買，使用pairBack資產balanceBack
-            let amount = balanceBack * amountPercent / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
-            amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
-            lastBuyAmount = amount;
-            order("BUY", amount, currentPrice); //下單
-            buy = true; //已做多
-            watchBuy = true; //開始監視此多單
-        }
+            /**
+             * 上唇 > 齒 > 下巴 & 下分形 > 下巴，為相對低點；做多
+             */
+            if (alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && fractalDown > alligatorDown && buy == false) {
+                console.log("上唇 > 齒 > 下巴 & 下分形 > 下巴，為相對低點；做多");
+                //多單要用pairBack買，使用pairBack資產balanceBack
+                let amount = balanceBack * amountPercent / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
+                amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
+                lastBuyAmount = amount;
+                order("BUY", amount, currentPrice); //下單
+                buy = true; //已做多
+                watchBuy = true; //開始監視此多單
+            }
 
-        /**
-         * 上唇 > 齒 > 下巴 & 當前價格 > 上分形，為強升趨勢；做多
-         */
-        if (alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && currentPrice > fractalUp && buy == false) {
-            console.log("上唇 > 齒 > 下巴 & 當前價格 > 上分形，為強升趨勢；做多");
-            //多單要用pairBack買，使用pairBack資產balanceBack
-            let amount = balanceBack * amountPercent / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
-            amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
-            lastBuyAmount = amount;
-            order("BUY", amount, currentPrice); //下單
-            buy = true; //已做多
-            watchBuy = true; //開始監視此多單
+            /**
+             * 上唇 > 齒 > 下巴 & 當前價格 > 上分形，為強升趨勢；做多
+             */
+            if (alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && currentPrice > fractalUp && buy == false) {
+                console.log("上唇 > 齒 > 下巴 & 當前價格 > 上分形，為強升趨勢；做多");
+                //多單要用pairBack買，使用pairBack資產balanceBack
+                let amount = balanceBack * amountPercent / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
+                amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
+                lastBuyAmount = amount;
+                order("BUY", amount, currentPrice); //下單
+                buy = true; //已做多
+                watchBuy = true; //開始監視此多單
+            }
         }
     });
 }, timeLoop);
-
-async function get(option) {
-    return await new Promise((resolve, reject) => {
-        request.get(
-            option,
-            (error, response, body) => {
-                resolve(JSON.parse(body, 0, 2));
-            }
-        );
-    });
-}
-
-async function post(option) {
-    return await new Promise((resolve, reject) => {
-        request.post(
-            option,
-            (error, response, body) => {
-                resolve(JSON.parse(body, 0, 2));
-            }
-        );
-    });
-}
-
-async function del(option) {
-    return await new Promise((resolve, reject) => {
-        request.delete(
-            option,
-            (error, response, body) => {
-                resolve(JSON.parse(body, 0, 2));
-            }
-        );
-    });
-}
 
 /**
  * 計算總資產，更動totalBalance
@@ -228,5 +197,38 @@ function order(action, amount, price) {
         type: "LIMIT"
     })).then((resData) => {
         console.log(resData);
+    });
+}
+
+async function get(option) {
+    return await new Promise((resolve, reject) => {
+        request.get(
+            option,
+            (error, response, body) => {
+                resolve(JSON.parse(body, 0, 2));
+            }
+        );
+    });
+}
+
+async function post(option) {
+    return await new Promise((resolve, reject) => {
+        request.post(
+            option,
+            (error, response, body) => {
+                resolve(JSON.parse(body, 0, 2));
+            }
+        );
+    });
+}
+
+async function del(option) {
+    return await new Promise((resolve, reject) => {
+        request.delete(
+            option,
+            (error, response, body) => {
+                resolve(JSON.parse(body, 0, 2));
+            }
+        );
     });
 }
