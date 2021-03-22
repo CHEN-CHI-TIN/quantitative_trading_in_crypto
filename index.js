68aabb0ee765fa6b19ba628b9ebc7dc7dfaa6051
CHEN-CHI-TIN <59937482+CHEN-CHI-TIN@users.noreply.github.com>
2021-03-22 18:34:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const email = "timmybrick@icloud.com";
 const apiKey = "2a4f6d473376f0dd36baf629bcd1d21b";
 const apiSecret = "$2a$12$YW1s2uH1Fpn1rThxfYf9LOSQ53DkVb8VqiT4/rlj/o/RI9TZYHCRi";
 const pairBack = "twd"; //基礎幣種
-const timeLoop = 1000 * 60; //更新頻率(毫秒)
+const timeLoop = 1000 * 15; //更新頻率(毫秒)
 const resolution = "30m"; //K線單位
 const timeAmount = 10; //資料量(多少時間內)
 const timeUnit = "MIN"; //資料量之時間單位
@@ -22,6 +22,9 @@ let switchPair_01 = true;
 let switchPair_02 = false;
 let switchPair_03 = false;
 let bear = false;
+let bear_01 = false;
+let bear_02 = false;
+let bear_03 = false;
 let buy = false;
 let watchBuy = false;
 let watchOffsetBuy = false;
@@ -29,7 +32,6 @@ let pairFront = "";
 
 setInterval(() => {
     if (lockPair == false) {
-        bear = false;
         if (switchPair_01 == true) {
             pairFront = pair_01;
             switchPair_01 = false;
@@ -64,15 +66,35 @@ setInterval(() => {
             let alligatorDown = o.toolRound(alligator["alligatorDown"], alligatorRound); //下巴(SMA13)
             let alligatorMiddel = o.toolRound(alligator["alligatorMiddel"], alligatorRound); //牙齒(SMA8)
             let alligatorUp = o.toolRound(alligator["alligatorUp"], alligatorRound); //上唇(SMA5)
-            let alligatorMax = Math.max(alligatorDown, alligatorMiddel, alligatorUp); //鱷魚線最大值
-            let alligatorMin = Math.min(alligatorDown, alligatorMiddel, alligatorUp); //鱷魚線最小值
+            // let alligatorMax = Math.max(alligatorDown, alligatorMiddel, alligatorUp); //鱷魚線最大值
+            // let alligatorMin = Math.min(alligatorDown, alligatorMiddel, alligatorUp); //鱷魚線最小值
             let currentPrice = alligator["currentPrice"]; //當前價格
+
+            console.log("--------------------------------------------------", "|", Date());
+            console.log("pairFront：", pairFront, "|", "currentPrice：", currentPrice);
+            console.log("fractalUp：", fractalUp, "|", "fractalDown：", fractalDown);
+            console.log("alligatorDown：", alligatorDown, "|", "alligatorMiddel：", alligatorMiddel, "|", "alligatorUp：", alligatorUp);
 
             /**
              * 上分形、下分形 < 下巴，處於熊市觀望做多訊號
              */
             if (fractalUp < alligatorDown && fractalDown < alligatorDown) {
+                if (pairFront == pair_01) {
+                    bear_01 = true;
+                } else if (pairFront == pair_02) {
+                    bear_02 = true;
+                } else if (pairFront == pair_03) {
+                    bear_03 = true;
+                }
+            }
+            if (pairFront == pair_01 && bear_01 == true) {
                 bear = true;
+            } else if (pairFront == pair_02 && bear_02 == true) {
+                bear = true;
+            } else if (pairFront == pair_03 && bear_03 == true) {
+                bear = true;
+            } else {
+                bear = false;
             }
 
             /**
@@ -91,14 +113,12 @@ setInterval(() => {
                             identity: email,
                             nonce: Date.now()
                         })).then((resData) => {
-                            console.log(Date());
                             console.log("刪除多頭平倉之漏單");
                             console.log(resData);
                             buy = true; //重新多頭平倉之流程
                             watchOffsetBuy = false;
                         });
                     } else {
-                        console.log(Date());
                         console.log("確認已多頭平倉");
                         watchOffsetBuy = false;
                         lockPair = false; //解除鎖定幣種
@@ -122,7 +142,6 @@ setInterval(() => {
                             identity: email,
                             nonce: Date.now()
                         })).then((resData) => {
-                            console.log(Date());
                             console.log("刪除做多之漏單");
                             console.log(resData);
                             buy = false; //重新做多之流程
@@ -130,20 +149,21 @@ setInterval(() => {
                             lockPair = false; //解除鎖定幣種，使下一循環開始為新幣種
                         });
                     } else {
-                        console.log(Date());
                         console.log("確認已做多");
                         watchBuy = false;
                         lockPair = true; //若已確認做多，鎖定幣種直至平倉
+                        bear_01 = false;
+                        bear_02 = false;
+                        bear_03 = false;
                     }
                 });
             }
 
             /**
-             * 上分形 > 鱷魚線 & 下分形 < 鱷魚線 & 下巴 < 齒、上唇，糾纏狀態，多頭平倉
+             * 上唇、齒 > 下巴 & 當前價格 < 下分形，多頭平倉
              */
-            if (fractalUp > alligatorMax && fractalDown < alligatorMin && alligatorDown < alligatorMiddel && alligatorDown < alligatorUp && buy == true && watchBuy == false) {
-                console.log(Date());
-                console.log("上分形 > 鱷魚線 & 下分形 < 鱷魚線，多頭平倉");
+            if (alligatorUp > alligatorDown && alligatorMiddel > alligatorDown && currentPrice < fractalDown && buy == true && watchBuy == false) {
+                console.log("上唇、齒 > 下巴 & 當前價格 < 下分形，多頭平倉");
                 //平倉要用pairFront賣，使用pairFront資產balanceFront
                 let amount = balanceFront * amountPercent; //每次購買amountPercent
                 amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
@@ -153,11 +173,10 @@ setInterval(() => {
             }
 
             /**
-             * 處於熊市 & 當前價格 > 鱷魚線，做多
+             * 處於熊市 & 當前價格 > 上分形，做多
              */
-            if (bear == true && currentPrice > alligatorMax && buy == false) {
-                console.log(Date());
-                console.log("處於熊市 & 當前價格 > 鱷魚線，做多");
+            if (bear == true && currentPrice > fractalUp && buy == false) {
+                console.log("處於熊市 & 當前價格 > 上分形，做多");
                 //多單要用pairBack買，使用pairBack資產balanceBack
                 let amount = balanceBack * amountPercent / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
                 amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
