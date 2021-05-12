efc6708e77d668fdf5a3b31243a86db53e8eb7e2
CHEN-CHI-TIN <59937482+CHEN-CHI-TIN@users.noreply.github.com>
2021-05-13 03:07:04 +0800

    update

--- index.js
+++ index.js
@@ -12,32 +12,20 @@ const timeUnit = "MIN"; //資料量之時間單位
 const alligatorRound = 4; //鱷魚線數值之四捨五入至小數點後?位
 const amountSize = 1; //每次交易量(1為全部)
 const amountRound = 8; //下單量之小數點位數
-const goal = 100; //收益超過總資產之?將平倉(%)
+const goal = 1; //收益超過總資產之?將平倉(%)
 
 const pair_01 = "btc";
-const pair_02 = "eth";
-const pair_03 = "bnb";
-const pair_04 = "ltc";
-const pair_05 = "trx";
+const pair_02 = "usdt";
+const pair_03 = "eth";
 
 let lockPair = false;
 let switchPair_01 = true;
 let switchPair_02 = false;
 let switchPair_03 = false;
-let switchPair_04 = false;
-let switchPair_05 = false;
 let bear = false;
 let bear_01 = false;
 let bear_02 = false;
 let bear_03 = false;
-let bear_04 = false;
-let bear_05 = false;
-let chaos = false;
-let chaos_01 = false;
-let chaos_02 = false;
-let chaos_03 = false;
-let chaos_04 = false;
-let chaos_05 = false;
 let buy = false;
 let watchBuy = false;
 let watchOffsetBuy = false;
@@ -57,14 +45,6 @@ setInterval(() => {
         } else if (switchPair_03 == true) {
             pairFront = pair_03;
             switchPair_03 = false;
-            switchPair_04 = true;
-        } else if (switchPair_04 == true) {
-            pairFront = pair_04;
-            switchPair_04 = false;
-            switchPair_05 = true;
-        } else if (switchPair_05 == true) {
-            pairFront = pair_05;
-            switchPair_05 = false;
             switchPair_01 = true;
         }
     }
@@ -94,19 +74,15 @@ setInterval(() => {
             let balanceCurrent = Math.floor(balanceBack + balanceFront * currentPrice); //當前總資產(無條件捨去)
 
             /**
-             * !下巴 > 齒 > 上唇 & 上分形 < 下巴，處於熊市
+             * 下巴 > 齒 > 上唇 & 上分形、下分形 < 下巴，處於熊市
              */
-            if (alligatorDown > alligatorMiddel && alligatorMiddel > alligatorUp && fractalUp < alligatorDown) {
+            if (alligatorDown > alligatorMiddel && alligatorMiddel > alligatorUp && fractalUp < alligatorDown && fractalDown < alligatorDown) {
                 if (pairFront == pair_01) {
                     bear_01 = true;
                 } else if (pairFront == pair_02) {
                     bear_02 = true;
                 } else if (pairFront == pair_03) {
                     bear_03 = true;
-                } else if (pairFront == pair_04) {
-                    bear_04 = true;
-                } else if (pairFront == pair_05) {
-                    bear_05 = true;
                 }
             }
             if (pairFront == pair_01 && bear_01 == true) {
@@ -115,59 +91,8 @@ setInterval(() => {
                 bear = true;
             } else if (pairFront == pair_03 && bear_03 == true) {
                 bear = true;
-            } else if (pairFront == pair_04 && bear_04 == true) {
-                bear = true;
-            } else if (pairFront == pair_05 && bear_05 == true) {
-                bear = true;
             } else {
                 bear = false;
-            }
-
-            /**
-             * !觀察熊市後之變化
-             * !上分形 > 鱷魚線 & 下分形 < 鱷魚線，處於熊市後之混沌期
-             */
-            if (bear == true && fractalUp > alligatorMax && fractalDown < alligatorMin) {
-                if (pairFront == pair_01) {
-                    chaos_01 = true;
-                } else if (pairFront == pair_02) {
-                    chaos_02 = true;
-                } else if (pairFront == pair_03) {
-                    chaos_03 = true;
-                } else if (pairFront == pair_04) {
-                    chaos_04 = true;
-                } else if (pairFront == pair_05) {
-                    chaos_05 = true;
-                }
-            }
-            if (pairFront == pair_01 && chaos_01 == true) {
-                chaos = true;
-            } else if (pairFront == pair_02 && chaos_02 == true) {
-                chaos = true;
-            } else if (pairFront == pair_03 && chaos_03 == true) {
-                chaos = true;
-            } else if (pairFront == pair_04 && chaos_04 == true) {
-                chaos = true;
-            } else if (pairFront == pair_05 && chaos_05 == true) {
-                chaos = true;
-            } else {
-                chaos = false;
-            }
-
-            /**
-             * !若處於混沌期後再次熊市(一切重置)
-             */
-            if (chaos == true && alligatorDown > alligatorMiddel && alligatorMiddel > alligatorUp && fractalUp < alligatorDown) {
-                bear_01 = false;
-                bear_02 = false;
-                bear_03 = false;
-                bear_04 = false;
-                bear_05 = false;
-                chaos_01 = false;
-                chaos_02 = false;
-                chaos_03 = false;
-                chaos_04 = false;
-                chaos_05 = false;
             }
 
             // console.log("--------------------------------------------------", "|", Date());
@@ -177,7 +102,7 @@ setInterval(() => {
             // console.log("alligatorDown：", alligatorDown, "|", "alligatorMiddel：", alligatorMiddel, "|", "alligatorUp：", alligatorUp);
 
             /**
-             * !追蹤多頭平倉之空單
+             * 追蹤多頭平倉之空單
              */
             if (watchOffsetBuy == true) {
                 //查看所有合約
@@ -192,14 +117,14 @@ setInterval(() => {
                             identity: email,
                             nonce: Date.now()
                         })).then((resData) => {
-                            console.log("--------------------------------------------------", Date());
+                            console.log("--------------------------------------------------", "|", Date());
                             console.log("刪除多頭平倉之漏單");
                             console.log(resData);
                             buy = true; //重新多頭平倉之流程
                             watchOffsetBuy = false;
                         });
                     } else {
-                        console.log("--------------------------------------------------", Date());
+                        console.log("--------------------------------------------------", "|", Date());
                         console.log("確認已多頭平倉");
                         watchOffsetBuy = false;
                         lockPair = false; //解除鎖定幣種
@@ -208,7 +133,7 @@ setInterval(() => {
             }
 
             /**
-             * !追蹤做多之多單
+             * 追蹤做多之多單
              */
             if (watchBuy == true) {
                 //查看所有合約
@@ -223,7 +148,7 @@ setInterval(() => {
                             identity: email,
                             nonce: Date.now()
                         })).then((resData) => {
-                            console.log("--------------------------------------------------", Date());
+                            console.log("--------------------------------------------------", "|", Date());
                             console.log("刪除做多之漏單");
                             console.log(resData);
                             buy = false; //重新做多之流程
@@ -231,29 +156,22 @@ setInterval(() => {
                             lockPair = false; //解除鎖定幣種，使下一循環開始為新幣種
                         });
                     } else {
-                        console.log("--------------------------------------------------", Date());
+                        console.log("--------------------------------------------------", "|", Date());
                         console.log("確認已做多");
                         watchBuy = false;
                         lockPair = true; //若已確認做多，鎖定幣種直至平倉
                         bear_01 = false;
                         bear_02 = false;
                         bear_03 = false;
-                        bear_04 = false;
-                        bear_05 = false;
-                        chaos_01 = false;
-                        chaos_02 = false;
-                        chaos_03 = false;
-                        chaos_04 = false;
-                        chaos_05 = false;
                     }
                 });
             }
 
             /**
-             * !收益超過goal，多頭平倉
+             * 收益超過goal，多頭平倉
              */
             if (balanceCurrent - balanceBeforeBuy >= balanceBeforeBuy / 100 * goal && buy == true && watchBuy == false) {
-                console.log("--------------------------------------------------", Date());
+                console.log("--------------------------------------------------", "|", Date());
                 console.log("收益超過goal，多頭平倉");
                 //平倉要用pairFront賣，使用pairFront資產balanceFront
                 let amount = balanceFront * amountSize; //每次購買amountPercent
@@ -264,11 +182,11 @@ setInterval(() => {
             }
 
             /**
-             * !當前價格 < 下分形 & 當前價格 < 鱷魚線，多頭平倉
+             * 熊市，多頭平倉
              */
-            if (currentPrice < fractalDown && currentPrice < alligatorMin && buy == true && watchBuy == false) {
-                console.log("--------------------------------------------------", Date());
-                console.log("當前價格 < 下分形 & 當前價格 < 鱷魚線，多頭平倉");
+            if (bear == true && buy == true && watchBuy == false) {
+                console.log("--------------------------------------------------", "|", Date());
+                console.log("熊市，多頭平倉");
                 //平倉要用pairFront賣，使用pairFront資產balanceFront
                 let amount = balanceFront * amountSize; //每次購買amountPercent
                 amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
@@ -278,11 +196,11 @@ setInterval(() => {
             }
 
             /**
-             * !已處於熊市後之混沌期 & 上唇 > 齒 > 下巴 & 下分形 > 下巴 & 下分形 < 上唇，做多
+             * 已處於熊市 & 上唇 > 齒 > 下巴 & 上分形、下分形 > 下巴，做多
              */
-            if (chaos == true && alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && fractalDown > alligatorDown && fractalDown < alligatorUp && buy == false) {
-                console.log("--------------------------------------------------", Date());
-                console.log("已處於熊市後之混沌期 & 上唇 > 齒 > 下巴 & 下分形 > 下巴 & 下分形 < 上唇，做多");
+            if (bear == true && alligatorUp > alligatorMiddel && alligatorMiddel > alligatorDown && fractalUp > alligatorDown && fractalDown > alligatorDown && buy == false) {
+                console.log("--------------------------------------------------", "|", Date());
+                console.log("已處於熊市 & 上唇 > 齒 > 下巴 & 上分形、下分形 > 下巴，做多");
                 //多單要用pairBack買，使用pairBack資產balanceBack
                 let amount = balanceBack * amountSize / currentPrice; //每次購買amountPercent，因使用pairFront匯率，故除於currentPrice
                 amount = o.toolRound(amount, amountRound); //四捨五入至amountRound位
@@ -310,6 +228,7 @@ function order(action, amount, price) {
         timestamp: Date.parse(new Date()),
         type: "LIMIT"
     })).then((resData) => {
+        console.log("--------------------------------------------------", "|", Date());
         console.log(resData);
     });
 }
